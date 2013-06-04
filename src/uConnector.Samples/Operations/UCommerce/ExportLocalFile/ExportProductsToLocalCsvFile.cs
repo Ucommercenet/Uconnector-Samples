57dc42669f282011f9aa3cd3fa78e02c6b0795f3
jesper nielsen <jesper.nielsen@ucommerce.dk>
2013-06-04 15:34:33 +0200

    Added two new ExportProductsToLocalFile operations.

---
+++ src/uConnector.Samples/Operations/UCommerce/ExportLocalFile/ExportProductsToLocalCsvFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UConnector.Config.Fluent.V1;
+using UConnector.Extensions.Senders;
+using UConnector.Extensions.Transformers;
+using UConnector.Samples.Operations.UCommerce.ExportProductListToFtp.Receiver;
+using UConnector.Samples.Operations.UCommerce.ExportProductListToFtp.Transformers;
+
+namespace UConnector.Samples.Operations.UCommerce.ExportLocalFile
+{
+	public class ExportProductsToLocalCsvFile : Operation
+	{
+		protected override IOperation BuildOperation()
+		{
+			return FluentOperationBuilder
+				.Receive<ProductListFromUCommerce>()
+				.Transform<ProductListToDataTable>()
+				.Transform<FromDataTableToCsvStream>()
+				.Transform<StreamToWorkfileWithTimestampName>()
+					.WithOption(a => a.Extension = ".csv")
+				.Batch(size: 1)
+				.Send<FilesToLocalDirectory>()
+					.WithOption(x => x.Overwrite = true)
+				.ToOperation();
+		}
+	}
+}
