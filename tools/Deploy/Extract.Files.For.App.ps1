9e22011a2ac06ee3ee3223e2901c534de01258ea
Mads Due <devnull@localhost>
2016-01-15 22:38:57 +0100

    Added script to build uCommerce.uConnector App when building the project.

---
+++ tools/Deploy/Extract.Files.For.App.ps1
@@ -0,0 +1,93 @@
+properties {
+  $TargetDirectory = $TargetDirectory;
+  $SourceDirectory = $SourceDirectory;
+}
+
+function FileExtensionBlackList {
+  return "*.cd","*.cs","*.dll","*.xml","*obj*","*.pdb","*.csproj*","*.cache","*.orig", "app.config", "packages.config";  
+}
+
+function DllExtensionBlackList {
+  return 
+  "UCommerce.dll",
+  "UCommerce.Infrastructure.dll",
+  "Castle.Core.dll",
+  "Castle.Windsor.dll",
+  "ClientDependency.Core.dll",
+  "Esent.Interop.dll",
+  "FluentNHibernate.dll",
+  "ICSharpCode.NRefactory.dll",
+  "ICSharpCode.NRefactory.CSharp.dll",
+  "Iesi.Collections.dll",
+  "Jint.Raven.dll",
+  "log4net.dll",
+  "Lucene.Net.dll",
+  "Lucene.Net.Contrib.Spatial.NTS.dll",
+  "Microsoft.CompilerServices.AsyncTargetingPack.Net4.dll",
+  "Microsoft.WindowsAzure.Storage.dll",
+  "NHibernate.dll",
+  "Raven.Abstractions.dll",
+  "Raven.Client.Embedded.dll",
+  "Raven.Client.Lightweight.dll",
+  "Raven.Database.dll",
+  "ServiceStack.Common.dll",
+  "ServiceStack.dll",
+  "ServiceStack.Interfaces.dll",
+  "ServiceStack.ServiceInterface.dll",
+  "ServiceStack.Text.dll",
+  "Spatial4n.Core.NTS.dll";  
+}
+
+function GetFilesToCopy($path){
+	return Get-ChildItem $path -name -recurse -include *.* -exclude (FileExtensionBlackList);
+}
+
+function CopyFiles ($appDirectory) {	
+	$filesToCopy = GetFilesToCopy($SourceDirectory);
+	
+	foreach($fileToCopy in $filesToCopy)
+	{
+    $sourceFile = $SourceDirectory + "\" + $fileToCopy;
+		$targetFile = $appDirectory + "\" + $fileToCopy;
+		
+		# Create the folder structure and empty destination file,
+		New-Item -ItemType File -Path $targetFile -Force
+		Copy-Item $sourceFile $targetFile -Force
+	}
+}
+
+function GetDllesToCopy($path){
+	return Get-ChildItem $path -name -recurse -include "*.dll*","*.pdb*"  -exclude (DllExtensionBlackList);
+}
+
+function CopyDllToBin ($appDirectory) {    
+	$filesToCopy = GetDllesToCopy($SourceDirectory);
+	
+	foreach($fileToCopy in $filesToCopy)
+	{
+    if($fileToCopy -notlike '*obj*'){
+      $sourceFile = $SourceDirectory + $fileToCopy;
+      $targetFile = $appDirectory + "\" + $fileToCopy;	
+      		
+      # Create the folder structure and empty destination file,
+      New-Item -ItemType File -Path $targetFile -Force	
+      Copy-Item $sourceFile $targetFile -Force   
+    }
+	}
+}
+
+task Run-It {        
+	write-host 'Extracting app to' + $TargetDirectory;
+    
+  #Creates app directory
+  If (!(Test-Path $TargetDirectory)) {
+		write-host 'Creating directory: ' + $TargetDirectory;
+    New-Item $TargetDirectory -type directory 
+  }	
+	
+	CopyFiles($TargetDirectory);
+
+	CopyDllToBin($TargetDirectory);
+    
+  write-host 'Extracted app to' + $TargetDirectory;   
+}
