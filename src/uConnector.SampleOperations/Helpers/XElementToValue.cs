5bfa3ee16235c40d84cd6deb30b40dac247c3271
Lasse Eskildsen <leskil99@gmail.com>
2016-01-17 21:55:50 +0100

    New sample project

---
+++ src/uConnector.SampleOperations/Helpers/XElementToValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+using UConnector.Framework;
+
+namespace UConnector.SampleOperations.Helpers
+{
+	public class XElementToValue : ITransformer<XElement, string>
+	{
+		public string Execute(XElement input)
+		{
+			return input.Value;
+		}
+	}
+}
---
+++ src/uConnector.SampleOperations/Operations/SampleOperation.cs
@@ -0,0 +1,27 @@
+﻿using UConnector.Api.V1;
+using UConnector.Helpers;
+using UConnector.IO;
+using UConnector.SampleOperations.Helpers;
+
+namespace UConnector.SampleOperations.Operations
+{
+    public class SampleOperation : Operation
+    {
+        protected override IOperation BuildOperation()
+        {
+            return FluentOperationBuilder
+                .Receive<FilesFromLocalDirectory>()         // receives IEnumerable<WorkFile>
+                    .WithOption(x => x.Pattern = "*.xml")
+                .Debatch()                                  // IEnumerable<WorkFile> to WorkFile
+                .Transform<WorkFileToXDocument>()           // WorkFile to XDocument
+                .Transform<XDocumentToXElements>()          // XDocument to IEnumerable<XElement>
+                    .WithOption(x => x.DescendendsName = "author")
+                .Debatch()                                  // IEnumerable<XElement> to XElement
+                .Transform<XElementToValue>()               // XElement to string
+                .Batch()                                    // string to IEnumerable<string>
+                .Transform<DistinctFilter<string>>()        // IEnumerable<string> to IEnumerable<string>
+                .Send<StringsToFileInLocalDirectory>()      // sends IEnumerable<string> to a file.
+                .ToOperation();
+        }
+    }
+}
